2d264ac533ad8e8a922086f1a7b1382c123f3cfd
Danylo Kondratiev <knightpp@proton.me>
2022-08-03 19:23:18 +0000

    Fix general linear regression

--- chapter3/linear.py
+++ chapter3/linear.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from functools import reduce
 from math import sqrt
 import random as rnd
 from typing_extensions import Self
@@ -115,17 +114,42 @@ class GeneralizedLinearFn:
         self.y_intercept = y_intercept
 
     def apply(self, xs: np.ndarray) -> float:
-        y = 0.0
-        for i, w in enumerate(self.weights):
-            y += w*xs[i]
+        y = np.dot(self.weights, xs)
         return y + self.y_intercept
 
-    def square_trick(self, x: float, y: float, learning_rate: float):
-        predicted_y = self.apply(x)
+    def square_trick(self, features: np.ndarray, y: float, learning_rate: float):
+        predicted_y = self.apply(features)
 
         self.y_intercept += learning_rate * (y - predicted_y)
-        for i in range(len(self.weights)):
-            self.weights[i] += learning_rate * x * (y - predicted_y)
+        self.weights += learning_rate * features * (y - predicted_y)
+
+    def plot(self, color='grey', linewidth=0.7, alpha=1.0, starting=0, ending=8):
+        if len(self.weights) > 1:
+            raise Exception("couldn't plot data with dimensions > 1")
+
+        x = np.linspace(starting, ending, 1000)
+        pyplot.plot(x, self.y_intercept + self.weights[0]*x, alpha=alpha, linestyle='-',
+                    color=color, linewidth=linewidth)
+
+    def __str__(self) -> str:
+        return f"y-intercept = {self.y_intercept} weights = {self.weights}"
+
+
+def regression_general(features: np.ndarray, ys: np.ndarray, learning_rate=0.01, epochs=1000) -> GeneralizedLinearFn:
+    # plot only 2D plots
+    if len(features[0]) == 1:
+        utils.plot_points(features, ys)
+
+    fn = GeneralizedLinearFn(np.random.rand(
+        1, len(features[0]))[0], rnd.random())
+    for epoch in range(epochs):
+        if rnd.random() < 0.1:
+            fn.plot(color='pink', alpha=0.1)
+        i = rnd.randint(0, len(features) - 1)
+        xs, y = features[i], ys[i]
+        fn.square_trick(xs, y, learning_rate)
+    fn.plot()
+    return fn
 
 
 def regression(xs: list[float], ys: list[float], learning_rate=0.01, epochs=1000) -> LinearFn:
